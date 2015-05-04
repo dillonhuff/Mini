5c2586810021461be51a9799db4c2afef6244aea
Dillon Huff <dillonhuff@gmail.com>
2015-05-04 04:14:23 -0500

    made Mini a cabal project with simple executable

--- src/Main.hs
+++ src/Main.hs
@@ -14,13 +14,14 @@ import RuntimeEvaluation
 import SymbolTable
 import Syntax
 
-fileName = "/Users/dillon/Haskell/Mini/ExampleLib.lspc"
+--fileName = "/Users/dillon/Haskell/Mini/ExampleLib.lspc"
 
 main :: IO ()
 main = do
---  (fileName:rest) <- getArgs
+  (fileName:cResFileName:rest) <- getArgs
   fileContents <- readFile fileName
   putStrLn $ libSpecToCString fileName fileContents
+  writeFile cResFileName (libSpecToCString fileName fileContents)
 
 libSpecToCString fileName libSpecStr =
   let matOps = readLibSpec fileName libSpecStr in
@@ -35,21 +36,3 @@ readLibSpec fileName libSpecStr =
 matrixOpToCString matOp =
   prettyPrint 0 $ toCFunc $ convertToMini $ matrixOperationToMOp matOp
 
-{-  timeResults <- timeImplementations "" "tiny_test" (Just sanityCheckImpl) [testImpl]
-  putStrLn $ show timeResults
-
-sanityCheckImpl = operation "sanity_check" stSym $ block [load "b_reg" "b" (iConst 0) "",
-                                                    load "c_reg" "c" (iConst 1) "",
-                                                    plus "b_reg" "b_reg" "c_reg" "",
-                                                    store "b" (iConst 0) "b_reg" ""]
-
-testImpl = operation "testOp" stSym $ block [load "c_reg" "c" (iConst 1) "",
-                                                    load "b_reg" "b" (iConst 0) "",
-                                                    plus "b_reg" "b_reg" "c_reg" "",
-                                                    store "b" (iConst 0) "b_reg" ""]
-
-stSym = miniSymtab [("b_reg", symInfo (sReg double) local),
-                ("c_reg", symInfo (sReg double) local),
-                ("b", symInfo (buffer double (iConst 4)) arg),
-                ("c", symInfo (buffer double (iConst 4)) arg)]
--}
