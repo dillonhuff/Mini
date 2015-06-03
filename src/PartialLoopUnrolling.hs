bf2326d6599a32b05213977037af5363542f822f
Dillon Huff <dillonhuff@gmail.com>
2015-06-03 18:37:22 -0500

    partial loop unrolling seems to work for level 1 examples

--- src/PartialLoopUnrolling.hs
+++ src/PartialLoopUnrolling.hs
@@ -17,22 +17,24 @@ tryPartialyUnrollLoop n st =
     Just stmts -> stmts
     Nothing -> [st]
 
-partiallyUnrollLoop :: Int -> Statement a -> Maybe [Statement a]
+partiallyUnrollLoop :: Int -> Statement String -> Maybe [Statement String]
 partiallyUnrollLoop n st =
   case partiallyUnrollable n st of
     True -> Just $ partiallyUnrollBy n st
---      iterSpace <- allIterationsList (forStart st) (forInc st) (forEnd st)
---      Just $ L.concatMap (\i -> blockStatements $ subIExprInBlock i (forInductionVariable st) (forBody st)) iterSpace
     False -> Nothing
 
 partiallyUnrollable n st =
   isFor st && moreThanNIterations n st && unitIncrement st
 
 partiallyUnrollBy n st =
-  [mainLoop]
+  [mainLoop, residualLoop]
   where
     mainIVarName = forInductionVariable st
     mainIVar = iVar mainIVarName
-    mainLoop = for mainIVarName (forStart st) (iConst n) (iSub (forEnd st) (iConst n)) unrolledBody (label st)
+    mainEnd = (iAdd (iSub (forEnd st) (iConst n)) (iConst 1))
+    mainLoop = for mainIVarName (forStart st) (iConst n) mainEnd unrolledBody (label st)
     unrolledBody = block $ L.concatMap (\i -> blockStatements $ subIExprInBlock (iAdd mainIVar (iConst i)) mainIVarName (forBody st)) [0..(n - 1)]
-    
+    residualIVarName = mainIVarName
+    residualIVar = iVar residualIVarName
+    residualLoop = for residualIVarName residualIVar (iConst 1) (forEnd st) residualBody ((label st) ++ "_u")
+    residualBody = block $ blockStatements $ subIExprInBlock residualIVar mainIVarName (forBody st)
