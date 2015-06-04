53269b0760bc2cdffe2a08215737acd5962d47c0
Dillon Huff <dillonhuff@gmail.com>
2015-06-03 20:59:20 -0500

    Starting on tests for test case generation

---
+++ test/TestCaseGenerationTests.hs
@@ -0,0 +1,31 @@
+module TestCaseGenerationTests(allTestCaseGenerationTests) where
+
+import Control.Monad.Random
+import Data.List as L
+
+import IndexExpression
+import SymbolTable
+import Testing.TestCaseGeneration
+import TestUtils
+
+allTestCaseGenerationTests :: IO String
+allTestCaseGenerationTests = testFunctionM (testGenTestCases (3 :: Int) (100 :: Int)) symtabCases
+
+symtabCases =
+  L.map (\(x, y) -> (createSymtabFromTuple x, y))
+  [([("A", iVar "m", iVar "n", iVar "rs", iVar "cs")], True),
+   ([("x", iVar "m", iConst 1, iConst 1, iConst 1)], True),
+   ([("x", iVar "m", iConst 1, iConst 1, iConst 1),
+     ("a", iConst 1, iConst 1, iConst 1, iConst 1)], True),
+   ([("A", iVar "m", iVar "n", iVar "rs", iConst 1)], True),
+   ([("A", iVar "m", iVar "n", iConst 1, iVar "cs")], True)]
+
+createSymtabFromTuple namesAndLayouts =
+  mOpSymtab $ L.map (\(name, nr, nc, rs, cs) -> (name, mOpSymInfo arg doubleFloat (layout nr nc rs cs))) namesAndLayouts
+
+testGenTestCases :: (MonadRandom m, Random a, Num a) => a -> a -> MOpSymtab -> m Bool
+testGenTestCases lo hi st = do
+  cases <- genTestCases lo hi st
+  case cases of
+    [] -> return False
+    other -> return True
--- test/TestUtils.hs
+++ test/TestUtils.hs
@@ -1,5 +1,8 @@
-module TestUtils(testFunction) where
+module TestUtils(testFunction,
+                 testFunctionM) where
 
+import Control.Monad
+import Data.List as L
 import Test.HUnit
 
 testFunction func cases = runTestTT $ makeTestCases func cases
@@ -10,3 +13,14 @@ makeTestCases func cases =
 testCase func input expected =
   TestCase (assertEqual ("Input: " ++ show input) expected (func input))
 
+testFunctionM :: (Show a, Show b, Eq b, Monad m) => (a -> m b) -> [(a, b)] -> m String
+testFunctionM f cases = do
+  computedValues <- mapM f $ L.map fst cases
+  let expectedValues = L.map snd cases in
+    return $ showResults computedValues expectedValues
+
+showResults [] [] = "Done"
+showResults (c:cs) (e:es) =
+  case c == e of
+    True -> "Test passed" ++ "\n" ++ showResults cs es
+    False -> "***************\nFAILED, expected " ++ show e ++ " but got " ++ show c ++ "\n***************\n" ++ showResults cs es
