7d89c9d5c3ca56e483adfb4e645a500817669576
Dillon Huff <dillonhuff@gmail.com>
2015-06-07 00:17:20 -0500

    moving dependence analysis into subdirectory

--- src/Analysis/Dependence.hs
+++ src/Analysis/Dependence.hs
@@ -6,10 +6,10 @@ module Analysis.Dependence(isFlowDependent,
                           flowDependent, antiDependent,
                           dependenceGraphForVectorInnerLoop) where
 
-import Data.Graph.Inductive as G
 import Data.List as L
 import Data.Map as M
 
+import Analysis.Dependence.Graph
 import Core.IndexExpression
 import Core.MiniOperation
 import Core.MiniSyntax
@@ -55,50 +55,13 @@ statementDeps st others = L.concatMap (\other -> statementPairDeps st other) $ L
 
 statementPairDeps l r =
   case isFlowDependent [] l r of
-    True -> [(l, r, Flow)]
+    True -> [(l, r, flowDep)]
     False -> []
-
-data DependenceGraph a
-     = DependenceGraph (Map a Node) (Gr a Dependence)
-       deriving (Eq, Show)
-
-data Dependence
-  = Flow
-  | Anti
-  | Output
-  | Input
-    deriving (Eq, Ord, Show)
 
 dependenceGraphForVectorInnerLoop :: (Show a, Ord a) => Statement a -> DependenceGraph a
 dependenceGraphForVectorInnerLoop forLoopStmt =
   let stmts = nonLoopStatements forLoopStmt
-      labNodePairs = L.zip (L.map label stmts) [1..(length stmts)]
-      nodeLabPairs = L.map (\(x, y) -> (y, x)) labNodePairs
-      depTriples = computeDependencies stmts
-      labNodeMap = M.fromList labNodePairs
-      depEdges = L.map (\(l, r, d) -> (findNode r labNodeMap, findNode l labNodeMap, d)) depTriples in
-  DependenceGraph labNodeMap (insEdges depEdges $ insNodes nodeLabPairs G.empty)
-
-findNode :: (Show a, Ord a) => Statement a -> Map a Node -> Node
-findNode st m =
-  case M.lookup (label st) m of
-    Just n -> n
-    Nothing -> error $ "findNode: " ++ show st ++ " does not exist in " ++ show m
-
-graphNode :: (Ord a, Show a) => a -> DependenceGraph a -> Node
-graphNode l (DependenceGraph m _) =
-  case M.lookup l m of
-    Just n -> n
-    Nothing -> error $ "graphNode: " ++ show l ++ " does not exist in " ++ show m
-
-dependenciesOfType :: (Show a, Ord a) => Dependence -> a -> DependenceGraph a -> [Node]
-dependenciesOfType d l dg@(DependenceGraph m g) =
-  let deps = lsuc g (graphNode l dg) in
-  L.map fst $ L.filter (\(_, y) -> y == d) deps      
-
-dependentQuery :: (Show a, Ord a) => Dependence -> DependenceGraph a -> a -> a -> Bool
-dependentQuery d depGraph t s =
-  L.elem (graphNode t depGraph) (dependenciesOfType d s depGraph)
-
-flowDependent depGraph l1 l2 = dependentQuery Flow depGraph l1 l2
-antiDependent depGraph l1 l2 = dependentQuery Anti depGraph l2 l1
+      labels = L.map label stmts
+      depTriplesSt = computeDependencies stmts
+      depTriples = L.map (\(l, r, d) -> (label l, label r, d)) depTriplesSt in
+  dependenceGraph labels depTriples
---
+++ src/Analysis/Dependence/Graph.hs
@@ -0,0 +1,60 @@
+module Analysis.Dependence.Graph(DependenceGraph,
+                                 dependenceGraph,
+                                 Dependence,
+                                 flowDep, antiDep, outputDep, inputDep,
+                                 flowDependent, antiDependent) where
+
+import Data.Graph.Inductive as G
+import Data.List as L
+import Data.Map as M
+
+import Core.MiniSyntax
+
+data DependenceGraph a
+     = DependenceGraph (Map a Node) (Gr a Dependence)
+       deriving (Eq, Show)
+
+dependenceGraph :: (Show a, Ord a) => [a] -> [(a, a, Dependence)] -> DependenceGraph a
+dependenceGraph labels depTriples =
+  let labNodePairs = L.zip labels [1..(length labels)]
+      nodeLabPairs = L.map (\(x, y) -> (y, x)) labNodePairs
+      labNodeMap = M.fromList labNodePairs
+      depEdges = L.map (\(l, r, d) -> (findLabel r labNodeMap, findLabel l labNodeMap, d)) depTriples in
+  DependenceGraph labNodeMap (insEdges depEdges $ insNodes nodeLabPairs G.empty)
+
+data Dependence
+  = Flow
+  | Anti
+  | Output
+  | Input
+    deriving (Eq, Ord, Show)
+
+flowDep = Flow
+antiDep = Anti
+outputDep = Output
+inputDep = Input
+
+findLabel :: (Show a, Ord a) => a -> Map a Node -> Node
+findLabel l m =
+  case M.lookup l m of
+    Just n -> n
+    Nothing -> error $ "findLabel: " ++ show l ++ " does not exist in " ++ show m
+
+graphNode :: (Ord a, Show a) => a -> DependenceGraph a -> Node
+graphNode l (DependenceGraph m _) =
+  case M.lookup l m of
+    Just n -> n
+    Nothing -> error $ "graphNode: " ++ show l ++ " does not exist in " ++ show m
+
+dependenciesOfType :: (Show a, Ord a) => Dependence -> a -> DependenceGraph a -> [Node]
+dependenciesOfType d l dg@(DependenceGraph m g) =
+  let deps = lsuc g (graphNode l dg) in
+  L.map fst $ L.filter (\(_, y) -> y == d) deps      
+
+dependentQuery :: (Show a, Ord a) => Dependence -> DependenceGraph a -> a -> a -> Bool
+dependentQuery d depGraph t s =
+  L.elem (graphNode t depGraph) (dependenciesOfType d s depGraph)
+
+flowDependent depGraph l1 l2 = dependentQuery Flow depGraph l1 l2
+
+antiDependent depGraph l1 l2 = dependentQuery Anti depGraph l2 l1
