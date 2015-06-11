e91195b11d5ae8da8565a6c424d8ea31fd0c6dcf
Dillon Huff <dillonhuff@gmail.com>
2015-06-10 22:33:59 -0500

    more work on transformations for inner loop parallelization

--- src/Optimizations/InnerLoopParallelization.hs
+++ src/Optimizations/InnerLoopParallelization.hs
@@ -1,17 +1,19 @@
 module Optimizations.InnerLoopParallelization(parallelizeInnerLoops) where
 
 import Data.List as L
+import Data.Map as M
 
 import Analysis.Dependence.RegisterReduction
 import Core.IndexExpression
 import Core.MiniSyntax
+import Utils.MapUtils
 
 parallelizeInnerLoops parFactor b =
   transformBlock (tryToParallelizeInnerLoop (registerUsageInfo b) parFactor) b
 
 tryToParallelizeInnerLoop regUseInfo parFactor stmt =
   case isMapLoop regUseInfo stmt of
-    True -> error "tryToParallelizeInnerLoop"
+    True -> stmt
     False -> stmt
 
 isMapLoop usageInfo stmt =
@@ -24,7 +26,14 @@ isInnerMapLoop usageInfo stmt =
   noLoopCarriedFlowDependencies stmt
 
 registerUsageInfo b =
-  error "registerUsageInfo"
+  let stmts = L.concatMap nonLoopStatements $ blockStatements b
+      regLabelList = L.concatMap (\stmt -> L.map (\op -> (op, label stmt)) $ L.filter (\op -> not $ isBufferVal op) $ allOperands stmt) stmts in
+  L.foldl addRegToUseMap M.empty regLabelList
+
+addRegToUseMap m (reg, lab) =
+  case M.lookup reg m of
+    Just vals -> M.insert reg (lab:vals) m
+    Nothing -> M.insert reg [lab] m
 
 noDeeperLoops stmt =
   let nonLoopStmtsInBody = nonLoopStatements stmt
@@ -32,9 +41,16 @@ noDeeperLoops stmt =
   L.length (L.intersect nonLoopStmtsInBody stmtsInBody) == L.length stmtsInBody
 
 allRegistersWrittenAreOnlyUsedInLoop usageInfo stmt =
-  True
+  let bodyStmts = nonLoopStatements stmt
+      bodyStmtLabels = L.map label bodyStmts
+      registersWritten = L.filter (\op -> not $ isBufferVal op) $ L.map operandWritten bodyStmts in
+  L.and $ L.map (\reg -> noUsageOutside usageInfo bodyStmtLabels reg) registersWritten
+
+noUsageOutside usageInfo labels reg =
+  let allUseLabels = lookupF reg usageInfo in
+  L.and $ L.map (\l -> L.elem l labels) allUseLabels
 
 noLoopCarriedFlowDependencies stmt =
   case buildLoopDependenceGraph stmt of
-    Just g -> error "loop graph"
+    Just g -> True
     Nothing -> False
